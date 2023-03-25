24cbb7191d52e745ce6773eeaec028bdcd523eb0
Shota Bennett <shota.bennett@gmail.com>
2023-03-24 22:07:34 -0400

    Initial Project Dump

---
+++ BMPHandler.c
@@ -0,0 +1,4 @@
+//
+// Created by vboxuser on 3/24/23.
+//
+#include "BMPHandler.h"
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Image.h"
+#include "BMPHandler.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
